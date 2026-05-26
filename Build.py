b9b1e6c59c1beb86fd6599d1edf0ef4565b65ba5
Andweki <andweki@gmail.com>
2026-05-26 07:43:12 +0300

    string and operators

--- Build.py
+++ Build.py
@@ -1,11 +1,11 @@
 from email.mime import text
 
 
-#name="Nicholas Kaguo"
-#print(name)
+name="Nicholas Kaguo"
+print(name)
 
-input= input("enter your name: ")
-print("Hello " + input)
+favourite_food = input("what is your favourite food: ")
+print("Hello " + favourite_food + " is a great choice!")
 
 age= 25
 print("I am " + str(age) + " years old")
@@ -17,7 +17,7 @@ is_student= True
 print("I am a student? " + str(is_student))
 
 class_name= "python programming"
-print("I am taking a class called " + class_name.replace("python programming", "javascript programming").upper())
+print("I am taking a class called " + class_name.replace("python", "javascript").upper())
 
 fruits=["Apple","Banana","Orange","Grapes","Watermelon"]
 print(fruits)
